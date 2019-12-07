e225e5066a34d35baf99092a95f2867c6c629889
Mateusz Sulecki <mat.sulecki@gmail.com>
2019-12-07 09:34:44 +0100

    Cosmetics

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -19,7 +19,7 @@ function ResultsList(props) {
     )
     return (
         <>
-            <div className="results__header">Results</div>
+            <div className="results__header">All results</div>
             <ol>
                 {items.length === 0 ? <div>¯\_(ツ)_/¯¯</div> : items}
             </ol>
