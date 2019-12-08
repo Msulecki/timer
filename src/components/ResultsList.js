572564ba716cd73f004fa425d7a4e82f1911b5c8
Mateusz Sulecki <mat.sulecki@gmail.com>
2019-12-08 18:04:59 +0100

    cosmetics

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -3,6 +3,7 @@ import '../style/ResultsList.scss'
 
 function ResultsList(props) {
     let items = props.results
+
     items = props.results.map((item, index) => {
         return (
             <li key={item[0]}>{item[3]} {new Date(item[0]).toLocaleString()}
